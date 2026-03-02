3be54117708d6312ddbba4c1d5884eaedd1f3606
manishhsuthar <manishsuthar.dev@gmail.com>
2026-03-02 10:56:13 +0530

    server created

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import pg from 'pg';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('app is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
